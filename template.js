900cd3e28e2b4c153d6b1b82d8a161661133f08e
Madhav Kumar <madhavrkumar@gmail.com>
2019-09-23 20:34:29 -0400

    updated template to seed random instance

--- template.js
+++ template.js
@@ -1,15 +1,22 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
 const util = require('./util');
+let seed;
 
 const settings = {
-  dimensions: [2048, 2048]
+  dimensions: [2048, 2048],
+  suffix: seed
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
+    seed  = window.performance.now();
+    random.setSeed(seed)
+
+
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
     let wMargin = 0;
     let hMargin = 0;
 
