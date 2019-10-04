d8c0fc94c6bdcd08e3c15544690d9f85a27fb136
MadhavRKumar <madhavrkumar@gmail.com>
2019-10-04 14:28:58 -0400

    finished 2

--- 2.js
+++ 2.js
@@ -15,18 +15,64 @@ const sketch = () => {
     random.setSeed(seed)
 
 
-    context.fillStyle = 'white';
+    context.fillStyle = '#efefef';
     context.fillRect(0, 0, width, height);
+    let string = createRandomString(20);
+    let wMargin = width/10;
+    let hMargin = height/10;
 
-    let wMargin = 0;
-    let hMargin = 0;
+    context.strokeStyle = "#333333";
+
+
+    let word = convert(string);
+    let count = 10;
+
+    let rectW = (width-2*wMargin)/word.length;
+    let rectH = (height-2*hMargin) / count;
+    let inc = random.gaussian(rectH);
+    for(let y = hMargin; y < height-hMargin; y += inc) {
+    let x = random.gaussian(wMargin, 20);
 
     
-    
+    for(let i = 0; i < word.length; i++) {
+      let char = word[i];
+
+      if(char == "1") {
+        //context.fillRect(x, wMargin, rectW+give, rectH);
+        context.lineWidth = random.gaussian(5, 10);
+        context.beginPath();
+        util.drawLine({x, y:y- random.gaussian(inc/2, 10)}, {x, y: y + random.gaussian(inc, 50)}, {context});
+        context.stroke();
+        context.closePath();
+      }
+
+      x += random.gaussian(rectW, 5);
+    }
+
+    string = random.shuffle(string.split('')).join('');
+    word = convert(string);
+    inc = random.gaussian(rectH, 10);
+  }
 
 
     function convert(string) {
+      let ret = "";
+
+      for(let i = 0; i < string.length; i++) {
+        ret += string[i].charCodeAt(0).toString(2);
+      }
+      return ret;
       
+    }
+
+    function createRandomString(len) {
+      let result           = '';
+      let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      let charactersLength = characters.length;
+      for ( let i = 0; i < len; i++ ) {
+         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      return result;
     }
 
 
