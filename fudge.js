a363871da93d4d675fc2731115fb6cb173ec5aa2
MadhavRKumar <madhavrkumar@gmail.com>
2019-11-07 11:58:31 -0500

    rothko trial but not feeling it

---
+++ fudge.js
@@ -0,0 +1,111 @@
+const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
+const util = require('./util');
+const Color = require('canvas-sketch-util/color');
+let seed;
+seed  = window.performance.now();
+
+const settings = {
+  dimensions: [3300, 4200],
+  suffix: seed
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    random.setSeed(seed)
+
+    let palettes = [
+      {
+        bg: "#B10116",
+        layers: ["#1d297c", "#1A1D22"]
+      }
+    ]
+
+    let palette = util.getRandomElem(palettes);
+
+    context.fillStyle = palette.bg;
+    context.fillRect(0, 0, width, height);
+
+    let wMargin = width/6;
+    let hMargin = width/6;
+    
+    let effectiveHeight = height - 2*hMargin;
+    let effectiveWidth = width - 2*width;
+
+    /*
+    each Range has (min.x,min.y) and (max.x, max.y)
+    */
+
+
+    /// Constructing Ranges
+
+
+    let ranges = [];
+
+    let curMin = {x: wMargin, y:hMargin}
+  
+    while (curMin.y < height-hMargin) {
+      let curMax = {
+        x: width-wMargin,
+        y: curMin.y + random.gaussian(effectiveHeight/3, 200)
+      }
+
+      if(curMax.y < height){
+        ranges.push(makeRange(curMin, curMax))
+      }
+      let buffer = random.gaussian(width/30);
+      curMin = {
+        x: curMin.x,
+        y: curMax.y+buffer
+      };
+
+    }
+
+    for(range of ranges) {
+      let curHeight = range.max.y - range.min.y;
+      let minW = effectiveWidth/100,
+      maxW = effectiveWidth/10,
+      minH = curHeight/100,
+      maxH = curHeight/10;
+
+      for(layer of palette.layers) {
+        context.fillStyle = layer + "11";
+        for(let i = 0; i < random.gaussian(10000, 100); i++) {
+          let point =  range.random();
+          let w = util.getRandom(minW, maxW);
+          let h = util.getRandom(minH, maxH);
+
+          let x = point.x - w/2;
+          let y = point.y - h/2;
+          context.beginPath();
+          context.rect(x, y, w, h);
+          context.closePath();
+  
+          context.fill();
+        }  
+      }
+      
+    }    
+    
+
+
+
+function makeRange(min, max) {
+  return {
+    min,
+    max,
+    random: () => {
+      let x = util.getRandom(min.x, max.x);
+      let y = util.getRandom(min.y, max.y);
+
+      return {x,y}
+    }
+  }
+}
+
+
+
+  };
+};
+
+canvasSketch(sketch, settings);
