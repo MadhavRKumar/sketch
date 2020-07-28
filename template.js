55fedb5ebb0e2f78ecda196aae39cd01c79ed4c2
MadhavRKumar <MadhavRKumar@gmail.com>
2020-07-27 22:41:48 -0400

    fix template

--- template.js
+++ template.js
@@ -3,34 +3,26 @@ const random = require('canvas-sketch-util/random');
 const util = require('./util');
 const Color = require('canvas-sketch-util/color');
 let seed;
-seed  = window.performance.now();
+seed = window.performance.now();
 
 const settings = {
-  dimensions: [3600, 3600],
-  suffix: seed
+	dimensions: [3600, 3600],
+	suffix: seed
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
-    random.setSeed(seed)
+	return ({ context, width, height }) => {
+		random.setSeed(seed)
 
 
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+    	context.fillStyle = 'white';
+    	context.fillRect(0, 0, width, height);
 
-    let wMargin = 0;
-    let hMargin = 0;
-
-    
-    
+    	let wMargin = 0;
+    	let hMargin = 0;
 
 
-
-
-
-
-
-  };
+	};
 };
 
 canvasSketch(sketch, settings);
